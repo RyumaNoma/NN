4fbbbf63eff8b06c35dc0325e6d5e525c64cade7
RyumaNoma <noma11.develop@gmail.com>
2023-06-13 15:27:18 +0900

    [fix]引数渡し忘れの修正

--- DeepLearningFromScratch/NeuralNetwork.cpp
+++ DeepLearningFromScratch/NeuralNetwork.cpp
@@ -13,7 +13,7 @@ void NeuralNetwork::Add(std::shared_ptr<layer::Layer> layer)
 
 double NeuralNetwork::Fit(const Matrix& in, const Matrix& out)
 {
-	double loss = Inference(in);
+	double loss = Inference(in, out);
 	Gradient();
 	Update();
 	return loss;
